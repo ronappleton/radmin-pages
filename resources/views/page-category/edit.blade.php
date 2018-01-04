bdb3f33198c1cc0d4c9255b20633665b8e39ff6d
Ron Appleton <ron.appleton@gmail.com>
2018-01-04 13:17:17 +0000

    .

--- resources/views/page-category/edit.blade.php
+++ resources/views/page-category/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     {{ Form::model($model, ['route' => ['page-category.update', $model], 'method' => 'PUT']) }}
-    @include('radmin-pages::form')
+    @include('radmin-pages::page-category.form')
     <div class="form-group">
         {{ Form::submit('Change Category', ['class' => 'btn btn-sm btn-success form-control']) }}
     </div>
